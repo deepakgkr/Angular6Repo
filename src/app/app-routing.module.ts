5133fa67155d10b59719fd71e2bcdc1426ff3c6c
Deepak Gupta <deepakgkr8@gmail.com>
2020-03-01 02:50:01 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,21 @@ import { RouterModule, Routes } from '@angular/router';
 // Import the components so they can be referenced in routes
 import { CreateEmployeeComponent } from './employee/create-employee.component';
 import { ListEmployeesComponent } from './employee/list-employees.component';
+import { HomeComponent } from './home.component';
+import { PageNotFoundComponent } from './page-not-found.component';
 
 // The last route is the empty path route. This specifies
 // the route to redirect to if the client side path is empty.
 const appRoutes: Routes = [
+  // home route
+  { path: 'home', component: HomeComponent },
   { path: 'list', component: ListEmployeesComponent },
   { path: 'create', component: CreateEmployeeComponent },
   { path: 'edit/:id', component: CreateEmployeeComponent },
-  { path: '', redirectTo: '/list', pathMatch: 'full' }
+  // redirect to the home route if the client side route path is empty
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  // wild card route
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 // Pass the configured routes to the forRoot() method
